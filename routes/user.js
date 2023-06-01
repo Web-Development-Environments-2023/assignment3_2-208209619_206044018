8a81aea982fab81943509cdecd725787611878bf
reut mealem <rm33334@gmail.com>
2023-06-01 10:51:00 +0300

    changes_re

--- routes/user.js
+++ routes/user.js
@@ -25,11 +25,12 @@ router.use(async function (req, res, next) {
 /**
  * This path gets body with recipeId and save this recipe in the favorites list of the logged-in user
  */
-router.post('/favorites', async (req,res,next) => {
+router.post('/userFavoriteRecipes/:recipe_id/:recipe_type', async (req,res,next) => {
   try{
     const user_id = req.session.user_id;
-    const recipe_id = req.body.recipeId;
-    await user_utils.markAsFavorite(user_id,recipe_id);
+    const recipe_id = req.params.recipe_id;
+    const recipe_type = req.params.recipe_type;
+    await user_utils.markAsFavorite(user_id,recipe_id, recipe_type);
     res.status(200).send("The Recipe successfully saved as favorite");
     } catch(error){
     next(error);
@@ -39,13 +40,13 @@ router.post('/favorites', async (req,res,next) => {
 /**
  * This path returns the favorites recipes that were saved by the logged-in user
  */
-router.get('/favorites', async (req,res,next) => {
+router.get('/userFavoriteRecipes', async (req,res,next) => {
   try{
     const user_id = req.session.user_id;
     let favorite_recipes = {};
     const recipes_id = await user_utils.getFavoriteRecipes(user_id);
     let recipes_id_array = [];
-    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
+    recipes_id.map((element) => recipes_id_array.push([element.recipe_id, element.recipe_type])); //extracting the recipe ids and recipe_type into array
     const results = await recipe_utils.getRecipesPreview(recipes_id_array);
     res.status(200).send(results);
   } catch(error){
@@ -53,6 +54,28 @@ router.get('/favorites', async (req,res,next) => {
   }
 });
 
+router.get('/userPersonalRecipes', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    
+    const recipes_id = await user_utils.getPersonalRecipes(user_id);
+    res.status(200).send(recipes_id);
+  } catch(error){
+    next(error); 
+  }
+});
+
+
+router.get('/userFamilyRecipes', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    
+    const recipes_id = await user_utils.getFamilyRecipes(user_id);
+    res.status(200).send(recipes_id);
+  } catch(error){
+    next(error); 
+  }
+});
 /**
  * This path returns the last 3 recepies that were viewed by the logged-in user
  */
--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -1,18 +1,31 @@
 const DButils = require("./DButils");
+const rec_utils = require("./utils/recipes_utils");
 
-async function markAsFavorite(user_id, recipe_id){
-    await DButils.execQuery(`insert into FavoriteRecipes values ('${user_id}',${recipe_id})`);
+async function markAsFavorite(user_id, recipe_id, recipe_type){
+    await DButils.execQuery(`insert into FavoriteRecipes values ('${user_id}',${recipe_id}, ${recipe_type})`);
+    return;
 }
 
 async function getFavoriteRecipes(user_id){
-    const recipes_id = await DButils.execQuery(`select recipe_id from FavoriteRecipes where user_id='${user_id}'`);
+    const recipes_id = await DButils.execQuery(`SELECT recipe_id, recipe_type from FavoriteRecipes WHERE user_id='${user_id}'`);
     return recipes_id;
+}
+
+async function getPersonalRecipes(user_id){
+    const recipes_id = await DButils.execQuery(`SELECT recipe_id from PersonalRecipes WHERE user_id='${user_id}'`);
+    
+    return await rec_utils.handlePersonalRecipeById(recipes_id);
+}
+
+async function getFamilyRecipes(user_id){
+    const recipes_id = await DButils.execQuery(`SELECT recipe_id from FamilyRecipes WHERE user_id='${user_id}'`);
+    return await rec_utils.handleFamilyRecipeById(recipes_id);
 }
 
 
 async function getViewedRecipes(user_id,amount=0){
     if (amount==0){
-        const recipes = await DButils.execQuery(`select recipe_id from ViewedRecipes where user_id='${user_id}'`);
+        const recipes = await DButils.execQuery(`SELECT recipe_id from ViewedRecipes where user_id='${user_id}'`);
 
     }  
     else{
@@ -40,3 +53,6 @@ exports.markAsFavorite = markAsFavorite;
 exports.getFavoriteRecipes = getFavoriteRecipes;
 exports.getViewedRecipes = getViewedRecipes;
 exports.putViewedRecipes = putViewedRecipes;
+exports.getFamilyRecipes = getFamilyRecipes;
+exports.getPersonalRecipes = getPersonalRecipes;
+
