8a6c0932e95ce962dffb00f8dea11e518384eba2
lidoravital <lidoravi@post.bgu.ac.il>
2023-07-22 16:35:51 +0300

    documentation

--- routes/recipes.js
+++ routes/recipes.js
@@ -5,12 +5,15 @@ const recipes_utils = require("./utils/recipes_utils");
 router.get("/", (req, res) => res.send("im here"));
 
 
-
+/**
+ * Route to get a number of random recipes
+ */
 router.get("/generalRandomRecipes", async (req, res, next) => {
   try {
+    // Call the utility function to get a specified number of random recipes 
     const recipes = await recipes_utils.RandomRecipe(3);
-    console.log(recipes);
-    // res.send(recipes);
+
+    // Respond with the random recipes
     res.send({"recipes": recipes});
   } catch (error) {
     next(error);
@@ -18,17 +21,22 @@ router.get("/generalRandomRecipes", async (req, res, next) => {
 });
 
 
-//TODO EXAMPLE
+/**
+ * Route to search recipes based on different parameters
+ */
 router.get("/searchRecipes/:recipe_name/:amount_recipes/:sort/:cuisine/:diet/:intolerance", async (req, res, next) => {
   try {
-    console.log("search params: ",req.params);
+    // Extract the search parameters from the request 
     const recipe_name = req.params.recipe_name
     const amount_recipes = req.params.amount_recipes
     const sort = req.params.sort
     const cuisine = req.params.cuisine
     const diet = req.params.diet
     const intolerance = req.params.intolerance
+
+    // Call the utility function to search for recipes based on the given parameters 
     const recipes = await recipes_utils.searchRecipes(recipe_name,amount_recipes,sort, cuisine, diet, intolerance);
+    
     res.status(200);
     res.send({"recipes":recipes})
   } catch (error) {
